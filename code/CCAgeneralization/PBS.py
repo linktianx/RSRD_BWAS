bb8e6881010ead5e4a1cee5cf848e8a9cfe2b575
Xiaohan TIAN <44140536+linktianx@users.noreply.github.com>
2024-10-30 11:24:22 +0800

    Update PBS.py comments

--- code/CCAgeneralization/PBS.py
+++ code/CCAgeneralization/PBS.py
@@ -1,1 +1,3 @@
 # Description: This script used for extracting PBS in HCP-D and UKB datasets.
+# The detailed process is illustrated in Figure S6.
+# The complete code will be made publicly available upon publication
